be1b39977cf5bfeb0dde6a5633cad0eed0653627
Phil Lee <phillee@live.com>
2021-01-28 14:45:55 -0800

    Adding contacts.txt

---
+++ ContactLIst/Contact.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContactList
+{
+    public class Contact
+    {
+        public string Name;
+        public string Address;
+        public string Phone;
+
+        public override string ToString()
+        {
+            return (this.Name + "; " + this.Phone + "; " + this.Address);
+        }
+    }
+}
--- ContactLIst/Program.cs
+++ ContactLIst/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Collections;
 
 namespace ContactList
 {
     class Program
     {
+        static ArrayList _contactlist = null;
+
         static void Main(string[] args)
         {
+            _contactlist = new ArrayList();
+
             Console.WriteLine("Hello World!");
+
+            Contact c = new Contact();
+            c.Name = "Benjamin Franklin";
+            c.Address = "123 State St., Philadelphia, PA  10483";
+            c.Phone = "204-284-4837";
+            Console.WriteLine(c.ToString());
+            
+            _contactlist.Add(c);
+            //Console.WriteLine(_contactlist[0].ToString());
+
         }
     }
 }
