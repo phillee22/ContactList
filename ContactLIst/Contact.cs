09f5c2bb0750b086e967b50ecee3d7c28bfebfcd
Phil Lee <phillee@live.com>
2021-01-28 19:44:37 -0800

    Added user cmd processing and test list.

--- ContactLIst/Contact.cs
+++ ContactLIst/Contact.cs
@@ -10,6 +10,20 @@ namespace ContactList
         public string Address;
         public string Phone;
 
+        public Contact()
+        {
+            Name = string.Empty;
+            Address = string.Empty;
+            Phone = string.Empty;
+        }
+
+        public Contact(string Name, string Address, string Phone)
+        {
+            this.Name = Name;
+            this.Address = Address;
+            this.Phone = Phone;
+        }
+
         public override string ToString()
         {
             return (this.Name + "; " + this.Phone + "; " + this.Address);
--- ContactLIst/Program.cs
+++ ContactLIst/Program.cs
@@ -10,18 +10,107 @@ namespace ContactList
         static void Main(string[] args)
         {
             _contactlist = new ArrayList();
+            ProcessUserCommands();
+        }
 
-            Console.WriteLine("Hello World!");
-
+        static void AddContact()
+        {
             Contact c = new Contact();
-            c.Name = "Benjamin Franklin";
-            c.Address = "123 State St., Philadelphia, PA  10483";
-            c.Phone = "204-284-4837";
-            Console.WriteLine(c.ToString());
-            
-            _contactlist.Add(c);
-            //Console.WriteLine(_contactlist[0].ToString());
+            Console.WriteLine("Adding new contact.");
 
+            // BUG:  not validating the input...
+            Console.Write(" Contact name: ");
+            c.Name = Console.ReadLine();
+
+            // BUG:  not validating the input...
+            Console.Write(" Contact address: ");
+            c.Address = Console.ReadLine();
+
+            // BUG:  not validating the input...
+            Console.Write(" Contact phone: ");
+            c.Phone = Console.ReadLine();
+
+            _contactlist.Add(c);
+            Console.WriteLine("  Contact added:  " + c.ToString());
+        }
+
+        static void CreateTestContacts()
+        {
+            Contact c = new Contact("Benjamin Franklin", "123 State St., Philadelphia, PA  10483", "394-284-4837");
+            _contactlist.Add(c);
+
+            c = new Contact("Betsy Anderson", "123 Main St. #4, New York, NY 10001", "212-555-1234");
+            _contactlist.Add(c);
+
+            c = new Contact("Alphie Preston", "149 E. 16th Ave., Sunnyvale, CA 94089", "408 - 555 - 6789");
+            _contactlist.Add(c);
+
+            c = new Contact("Gamal Abdel", "369 Center St., Boston, MA 02130", "617 - 555 - 1098");
+            _contactlist.Add(c);
+        }
+
+        private static void ListContacts()
+        {
+            Console.WriteLine("Here's the full list of contacts:");
+            Console.WriteLine();
+            foreach (Contact c in _contactlist)
+            {
+                Console.WriteLine("  " + c.ToString());
+            }
+        }
+
+        private static void ProcessUserCommands()
+        {
+            const string ADD = "a";
+            const string LIST = "l";
+            const string QUIT = "q";
+            string cmd = string.Empty;
+
+            ShowIntro();
+            while (cmd != QUIT)
+            {
+                // give the user the cursor so they know we're ready for input...
+                Console.Write(">");
+                cmd = Console.ReadLine();
+                switch (cmd)
+                {
+                    case ADD:
+                        AddContact();
+                        break;
+
+                    case LIST:
+                        ListContacts();
+                        break;
+
+                    case QUIT:
+                        SayGoodbye();
+                        break;
+                }
+            }
+        }
+
+        private static void SayGoodbye()
+        {
+            Console.WriteLine();
+            Console.WriteLine(">>>  Bye, bye.");
+            Console.WriteLine();
+        }
+
+        private static void ShowIntro()
+        {
+            Console.WriteLine("Hi.  Getting started...");
+            Console.WriteLine();
+
+            // Load the test list for now...
+            Console.WriteLine("Loading test list...");
+            CreateTestContacts();
+            Console.WriteLine();
+
+            Console.WriteLine("Please input a command:");
+            Console.WriteLine("  a: add a new contact");
+            Console.WriteLine("  l: list all contacts");
+            Console.WriteLine("  q: quit the app");
+            Console.WriteLine();
         }
     }
 }
